206b8ed7c327b255cff19baf1bbda20b7c78b106
Adrian A. Agcaoili <109133228+ZCMC-ADRIAN@users.noreply.github.com>
2023-01-25 17:01:19 +0800

    update

--- src/Components/InventoryModal.js
+++ src/Components/InventoryModal.js
@@ -7,29 +7,26 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  useDisclosure,
-  Button
+  Button,
+  Text,
+  Flex
 } from '@chakra-ui/react'
 
 const InventoryModal = ({isOpen, onClose}) => {
 
   return (
     <>
-      <Modal closeOnOverlayClick={true} isOpen={isOpen} onClose={onClose} size='2xl'>
+      <Modal closeOnOverlayClick={true} isOpen={isOpen} onClose={onClose} size='xl'>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader fontSize={'sm'} color='#2583CF'>Speaker System Stereo Megaoke</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            Details
+            <Flex justifyContent='space-evenly'>
+              <Text>Brand:</Text>
+              <Text>Sharp</Text>
+            </Flex>
           </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
-              Save
-            </Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
